dccf98b66bb3df4a0fbaa33f64121cad4246a155
takaodaze <gin2798@gmail.com>
2022-05-10 06:09:51 +0900

    add code

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { ErrorBoundary } from "./component/functional/ErrorBoundary";
 import { LearningInput } from "./component/LearningInput";
 import { LoginButton } from "./component/LoginButton";
+import { firebaseClient } from "./config/firebase";
 
 export const App = () => {
+    // 自動ログインに使おう
+    firebaseClient.auth.onAuthStateChanged((e) => {
+        console.log(e);
+    });
     return (
         <div className="h-screen w-full">
             <LoginButton />
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -25,7 +25,7 @@ const firebaseConfig = {
 class FirebaseClient {
     private app;
     private analytics;
-    private auth;
+    auth;
     private googleAuthProvider;
 
     constructor() {
