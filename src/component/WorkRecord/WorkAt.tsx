f16fb1f645a3df0405d0bba1c9024ba836cb27ab
takaodaze <gin2798@gmail.com>
2022-05-15 22:23:46 +0900

    fix error

--- src/component/WorkRecord/WorkAt.tsx
+++ src/component/WorkRecord/WorkAt.tsx
@@ -1,6 +1,8 @@
 import { AiOutlineCalendar } from "react-icons/ai";
 export const WorkAt = ({ workAt }: { workAt: Date }) => {
-    const display = `${workAt.getFullYear()}/${workAt.getMonth()}/${workAt.getDate()}`;
+    const display = `${workAt.getFullYear()}/${
+        workAt.getMonth() + 1
+    }/${workAt.getDate()}`;
     return (
         <div className="flex items-center gap-1">
             <AiOutlineCalendar />
