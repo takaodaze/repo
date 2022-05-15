d206dd316c2c2fe24f907d4fb3b7ae8eeeccc365
takaodaze <gin2798@gmail.com>
2022-05-15 22:28:30 +0900

    style

--- src/component/WorkRecord/WorkAt.tsx
+++ src/component/WorkRecord/WorkAt.tsx
@@ -1,8 +1,8 @@
 import { AiOutlineCalendar } from "react-icons/ai";
 export const WorkAt = ({ workAt }: { workAt: Date }) => {
-    const display = `${workAt.getFullYear()}/${
+    const display = `${workAt.getFullYear()}年 ${
         workAt.getMonth() + 1
-    }/${workAt.getDate()}`;
+    }月 ${workAt.getDate()}日`;
     return (
         <div className="flex items-center gap-1">
             <AiOutlineCalendar />
