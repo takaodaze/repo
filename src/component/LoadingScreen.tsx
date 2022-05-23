8e9c5a0973d49482b8b3b9e1d3faf0e03bf4f465
takaodaze <gin2798@gmail.com>
2022-05-24 03:17:36 +0900

    change style

--- src/component/LoadingScreen.tsx
+++ src/component/LoadingScreen.tsx
@@ -12,7 +12,7 @@ export const LoadingScreen = () => {
     return (
         <div
             style={{ zIndex: zIndex.Loading }}
-            className="absolute top-0 left-0 flex h-screen w-screen items-center justify-center bg-gray-300 shadow-xl "
+            className="absolute top-0 left-0 flex h-screen w-screen items-center justify-center bg-gray-500 shadow-xl "
         >
             <div className="opacity-1 absolute flex items-center space-x-3 rounded-lg bg-white p-5 ">
                 <div className="h-7 w-7 animate-spin rounded-full border-4 border-green-400 border-t-transparent" />
