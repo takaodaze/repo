076fae55ac9968148712106250ddcf323d6ae15c
takaodaze <gin2798@gmail.com>
2022-05-31 00:02:09 +0900

    align center

--- src/component/Heaatmap/Heatmap.tsx
+++ src/component/Heaatmap/Heatmap.tsx
@@ -24,41 +24,44 @@ export const Heatmap = () => {
             className={`overflow-x-scroll ${classNameForScrollBar} rounded-lg border-2 p-4 dark:border-slate-700`}
         >
             <div className="h-12" />
-            <div className="relative grid w-fit grid-flow-col grid-rows-7 gap-0.5">
-                <div className="absolute -top-12 flex w-full flex-col gap-1">
-                    <div className="flex w-full justify-between text-sm font-bold">
-                        <div>{currentYear - 1}年</div>
-                        <div>{currentYear}年</div>
+            <div className="flex justify-center">
+                <div className="relative grid w-fit grid-flow-col grid-rows-7 gap-0.5">
+                    <div className="absolute -top-12 flex w-full flex-col gap-1">
+                        <div className="flex w-full justify-between text-sm font-bold">
+                            <div>{currentYear - 1}年</div>
+                            <div>{currentYear}年</div>
+                        </div>
+                        <div className="flex w-full justify-between px-3">
+                            {monthList.map((m, idx) => (
+                                <div
+                                    key={`${m}_${idx}`}
+                                    className="text-sm font-bold"
+                                >
+                                    {m}
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                    <div className="flex w-full justify-between px-3">
-                        {monthList.map((m, idx) => (
-                            <div
-                                key={`${m}_${idx}`}
-                                className="text-sm font-bold"
-                            >
-                                {m}
-                            </div>
-                        ))}
-                    </div>
+                    {repoDate365Array.map((r, idx) => {
+                        const workMinutes = workRecordList.reduce(
+                            (prev, curr) =>
+                                r.equals(RepoDate.fromDate(curr.workAt))
+                                    ? convWorkDurationToMinute(
+                                          curr.workDuration
+                                      ) + prev
+                                    : prev,
+                            0
+                        );
+                        return (
+                            <HeatmapCell
+                                key={`heatmap_cell_${idx}`}
+                                workMinutes={workMinutes}
+                                idx={idx}
+                                workAt={r}
+                            />
+                        );
+                    })}
                 </div>
-                {repoDate365Array.map((r, idx) => {
-                    const workMinutes = workRecordList.reduce(
-                        (prev, curr) =>
-                            r.equals(RepoDate.fromDate(curr.workAt))
-                                ? convWorkDurationToMinute(curr.workDuration) +
-                                  prev
-                                : prev,
-                        0
-                    );
-                    return (
-                        <HeatmapCell
-                            key={`heatmap_cell_${idx}`}
-                            workMinutes={workMinutes}
-                            idx={idx}
-                            workAt={r}
-                        />
-                    );
-                })}
             </div>
         </div>
     );
